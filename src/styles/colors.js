d37e70883fd64db2f6ed509c8348bb50a0a76106
sasha240100 <alexbuzin88@gmail.com>
2016-09-22 23:30:54 +0300

    Add color scheme

---
+++ src/styles/colors.js
@@ -0,0 +1,34 @@
+// UI COLOR SCHEME - WhitestormJS
+//
+// Defenition:
+//
+//   "$" sign - BACKGROUND
+//
+//   "_" sign - OUTLINE, TEXT
+//
+
+// Colors
+export const uiPurple = '#9b61e3';
+export const uiBlue = '#0099e5';
+export const uiGreen = '';
+export const uiOrange = '#ffae54';
+export const uiGreen = '#B8E361';
+
+// UI Dark
+export const uiDark$ = '#2D3134';
+export const uiDark_ = '#D0D4D7';
+export const uiDark_link = '#0099e5';
+export const uiDarkBlock$ = '#242729';
+export const uiDarkBlock_ = '#D0D4D7';
+export const uiDarkBox$ = '#272B2D';
+export const uiDarkBox$table = '#33373A';
+export const uiDarkBox$table2 = '#2E3234';
+export const uiDarkBox_ = '#D0D4D7';
+export const uiDarkField$ = '#2D3134';
+export const uiDarkField_ = '#666';
+
+// UI Light
+export const uiLight$ = '#fff';
+export const uiLight_ = '#4c555a';
+export const uiLight_tip = '#939da3';
+
