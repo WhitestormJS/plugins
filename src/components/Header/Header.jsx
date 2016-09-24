fa86b7d19bdd91956c93db69d8f2f4cb39bc84c6
sasha240100 <alexbuzin88@gmail.com>
2016-09-24 18:49:26 +0300

    Play with logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Card, CardActions} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import SearchBar from '../SearchBar';
@@ -22,13 +22,63 @@ const logoStyle = {
   height: 50,
   alignSelf: 'center',
   position: 'relative',
-  margin: '0 10px'
+  margin: '0 10px',
+  cursor: 'pointer'
 };
+
+class Logo extends Component {
+  constructor () {
+    super();
+    const _path = 'images/';
+
+    this.state = {
+      currentLogo: _path + 'logo_orange.png'
+    };
+
+    this.imageList = [
+      'logo_orange.png',
+      'logo_blue.png',
+      'logo_purple.png',
+      'logo_green.png'
+    ];
+
+    this.rareList = {
+      'logo_fish.png': 70,
+      'logo_bear.png': 100,
+      'logo_rare.png': 200,
+      'logo_veryrare.png': 350,
+      'logo_extremelyrare.png': 3000
+    }
+
+    this.changeImage = () => {
+      let newLogo = this.state.currentLogo;
+
+      while (newLogo === this.state.currentLogo) {
+        for (let key in this.rareList) {
+          const value = this.rareList[key];
+
+          if (Math.round(Math.random() * value) === value) {
+            newLogo = _path + key;
+          }
+        }
+
+        if (newLogo === this.state.currentLogo)
+          newLogo = _path + this.imageList[Math.round(Math.random() * 3)];
+      }
+
+      this.setState({currentLogo: newLogo});
+    }
+  }
+
+  render() {
+    return (<img src={this.state.currentLogo} style={logoStyle} onClick={this.changeImage}></img>)
+  }
+}
 
 function Header() {
   return (
     <Card style={style} containerStyle={containerStyle}>
-      <img src="images/logo.png" style={logoStyle}></img>
+      <Logo />
       <SearchBar />
       <CardActions style={{alignSelf: 'center'}}>
         <FlatButton label="Plugins" />
